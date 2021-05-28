f034b87414a463e65bb3da9b57694ca52ee3b204
AdroitMachine <anujsaxenamail@gmail.com>
2021-05-28 23:28:04 +0530

    updated

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import QuoteList from '../components/quotes/QuoteList'
+import LoadingSpinner from '../components/ui/LoadingSpinner'
+import NoQuotesFound from '../components/quotes/NoQuotesFound'
+import useHttp from '../hooks/use-http'
+import { getAllQuotes } from '../lib/api'
 
-const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Max', text: 'Learning react is fun!' },
-    { id: 'q2', author: 'Maxi', text: 'Learning node is fun!' },
-    { id: 'q1', author: 'Max', text: 'Learning javascript is fun!' },
-    { id: 'q1', author: 'Max', text: 'Learning something is fun!' },
+// const DUMMY_QUOTES = [
+//     { id: 'q1', author: 'Max', text: 'Learning react is fun!' },
+//     { id: 'q2', author: 'Maxi', text: 'Learning node is fun!' },
+//     { id: 'q1', author: 'Max', text: 'Learning javascript is fun!' },
+//     { id: 'q1', author: 'Max', text: 'Learning something is fun!' },
 
-]
+// ]
 const AllQuotes = () => {
+    const { sendRequest, status, data: loadedQuotes, error } = useHttp(getAllQuotes, true);
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest])
+    if (status === 'pending') {
+        return (<div className="centered">
+            <LoadingSpinner />
+        </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <p className="centered focused">{error}</p>
+        )
+
+    }
+    if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+        return <NoQuotesFound />
+    }
     return (
-        <QuoteList quotes={DUMMY_QUOTES} />
+        // <QuoteList quotes={DUMMY_QUOTES} />
+        <QuoteList quotes={loadedQuotes} />
     )
 }
 
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import { useParams, Route } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom'
 import HighlightedQuote from '../components/quotes/HighlightedQuote'
 import Comments from '../components/comments/Comments';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api'
+import LoadingSpinner from '../components/ui/LoadingSpinner';
 
 const DUMMY_QUOTES = [
     { id: 'q1', author: 'Max', text: 'Learning react is fun!' },
@@ -12,15 +15,42 @@ const DUMMY_QUOTES = [
 ]
 
 const QuoteDetails = () => {
+    const match = useRouteMatch();
     const params = useParams();
+    const { quoteId } = params;
+    const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
+
+    useEffect(() => {
+        sendRequest(quoteId);
+
+    }, [sendRequest, quoteId])
     const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
-    if (!quote) {
+
+    if (status === 'pending') {
+        return (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        )
+    }
+    if (error) {
+        return (
+            <p className="centered">{error}</p>
+        )
+    }
+    if (!loadedQuote.text) {
         return <p>No Quote Found!</p>
     }
     return (
         <>
-            <HighlightedQuote text={quote.text} author={quote.author} />
-            <Route path={`/quotes/${params.quoteId}/comments`}>
+            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+            <Route path={match.path} exact>
+                <div className="centered">
+                    <Link className="btn--flat" to={`${match.url}/comments`}>Load comments</Link>
+                </div>
+            </Route>
+
+            <Route path={`${match.path}/comments`}>
                 <Comments />
             </Route>
         </>
